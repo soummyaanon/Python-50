a2a03005441f2b076dfb01590846ba90bea4f9a9
soummyaanon <soumyaranjanpanda910@gmail.com>
2023-12-25 02:13:43 +0530

    Add Datatypes.py and pythoncast.py

---
+++ Datatypes.py
@@ -0,0 +1,35 @@
+# Integer
+integer_variable = 10
+print("Integer Variable:", integer_variable)
+
+# Float
+float_variable = 3.14
+print("Float Variable:", float_variable)
+
+# String
+string_variable = "Hello, Python!"
+print("String Variable:", string_variable)
+
+# Boolean
+boolean_variable = True
+print("Boolean Variable:", boolean_variable)
+
+# List
+list_variable = [1, 2, 3, 4, 5]
+print("List Variable:", list_variable)
+
+# Tuple
+tuple_variable = (10, 20, 30)
+print("Tuple Variable:", tuple_variable)
+
+# Dictionary
+dictionary_variable = {'key1': 'value1', 'key2': 'value2'}
+print("Dictionary Variable:", dictionary_variable)
+
+# Set
+set_variable = {1, 2, 3, 4, 5}
+print("Set Variable:", set_variable)
+
+# Complex
+complex_variable = 2 + 3j
+print("Complex Variable:", complex_variable)
