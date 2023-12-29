90f0eb0f337f330e7194a5246dd9bdb681ec29d6
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-12-30 01:00:59 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,22 @@
+# given upper bound
+num = 50
+ 
+# initial values in the series
+first_value,second_value = 0, 1
+ 
+# iterate in the given range
+# of numbers
+for n in range(0, num):
+ 
+    # if no. is less than 1
+    # move to next number
+    if(n <= 1):
+        next = n
+ 
+    # if number is within range
+    # execute the below code block
+    if nextnum:
+        break
+    # print each element that
+    # satisfies all the above conditions
+    print(next)
