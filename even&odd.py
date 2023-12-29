f238ca3fc98ac23225fa35e6633bc60436299b89
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-12-30 00:36:56 +0530

    Update even&odd.py

--- even&odd.py
+++ even&odd.py
@@ -1,1 +1,17 @@
-
+# given list of numbers
+num_list = [1,3,5,6,99,134,55]
+ 
+ 
+# iterate through the list elemets
+# using for loop
+for i in num_list:
+ 
+    # if divided by 2, all even
+    # number leave a remainder of 0
+    if i%2==0:
+        print(i,"is an even number.")
+ 
+    # if remainder is not zero
+    # then it's an odd number
+    else:
+        print(i,"is an odd number.")
