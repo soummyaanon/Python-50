474a097850a44f8138ad5e1a37c750a89935ef0d
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-02 02:21:19 +0530

    Add Python script for factorial calculation

---
+++ factorial.py
@@ -0,0 +1,13 @@
+# given number 
+given_number=5
+
+# since 1 is the factor of all number # set the factorial to 1
+
+factorial = 1
+
+#iterate till the given number 
+for i in range(1,given_number+1):
+    factorial = factorial*i
+
+
+print("The factoril of",given_number,"is",factorial)
