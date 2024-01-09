bc96795e8761664fd08667c0c6a9622384657cd2
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-09 21:55:00 +0530

    password checker

--- passwordvalidity.py
+++ passwordvalidity.py
@@ -1,30 +1,25 @@
 password = input()
 
 has_valid_length = False
-has_valid_case = False
+has_lower_case = False  # Added this variable
 has_upper_case = False
 has_digit = False
 has_special_characters = False
 
-if(len(password)>=13)and(len(password)<=16):
-
+if 13 <= len(password) <= 16:
     has_valid_length = True
 
     for i in password:
-
-        if(i.islower()):
+        if i.islower():
             has_lower_case = True
+        elif i.isupper():
+            has_upper_case = True
+        elif i.isdigit():
+            has_digit = True
+        elif i in ["@", "$", "#", "^", "&", "*"]:
+            has_special_characters = True
 
-            if(i.isupper()):
-                has_upper_case = True
-
-                if(i.isdigit()):
-                    has_digit = True
-
-                    if(i=="@" or i=="$" or i=="#" or i =="^" or i=="&" or i=="*" ):
-                        has_special_characters = True
-
-if (has_valid_length==True and has_lower_case ==True and has_upper_case == True and has_digits == True and has_special_characters == True):
+if has_valid_length and has_lower_case and has_upper_case and has_digit and has_special_characters:
     print("Valid Password")
 else:
-        print("Invalid Password")                        
+    print("Invalid Password")
