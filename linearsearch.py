5e1876e67b68fc2cb881f2bb270260c4ec6c58e6
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-24 01:07:16 +0530

    Update linearsearch.py

--- linearsearch.py
+++ linearsearch.py
@@ -4,5 +4,11 @@ def linear_search(array, target):
             return i
     return -1
 
-# Test the function
-print(linear_search([1, 2, 3, 4, 5], 3))
+# Prompt the user to enter the array as a list of integers
+array = [int(i) for i in input("Enter the array: ").split(',')]
+
+# Prompt the user to enter the target value
+target = int(input("Enter the target value: "))
+
+# Call the linear_search function and print the result
+print(linear_search(array, target))
