1770798a73605867993b0fca61aa9c24f8712929
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-29 01:01:34 +0530

    maximum of two number

---
+++ exp3.py
@@ -0,0 +1,10 @@
+def maximum (a,b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print(maximum(a,b)) # Call the function
