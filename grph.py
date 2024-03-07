62713c014e507a87bb05227e5c4120af1e4600d4
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-03-07 11:57:24 +0530

    Refactor pie chart code to display percentage values

--- grph.py
+++ grph.py
@@ -1,8 +1,12 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.pyplot import pie, axis, show
+
+import matplotlib.pyplot as plt
+
 df = pd.read_csv("/Users/soumyaram/Python-50/Doc.csv")
 sums = df.groupby(df["Name"])["Marks"].sum()
-pie(sums, labels=sums.index);
-show()
 
+# Plot the pie chart with percentage values
+pie(sums, labels=sums.index, autopct='%1.1f%%')
+
+show()
